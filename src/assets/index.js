ea77dd379164e8ee5e8166143a4bf1043f7b066c
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-02-26 07:41:25 -0300

    feat(ícones): adiciona ícones necessários e importa

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,29 @@
 import search from "./search.png";
+import favoritoAtivo from "../../public/icones/favorito-ativo.png";
+import favoritoInativo from "../../public/icones/favorito.png";
+import homeAtivo from "../../public/icones/home-ativo.png";
+import homeInativo from "../../public/icones/home-inativo.png";
+import maisCurtidasAtivo from "../../public/icones/mais-curtidas-ativo.png";
+import maisCurtidasInativo from "../../public/icones/mais-curtidas-inativo.png";
+import maisVistasAtivo from "../../public/icones/mais-vistas-ativo.png";
+import maisVistasInativo from "../../public/icones/mais-vistas-inativo.png";
+import novasAtivo from "../../public/icones/novas-ativo.png";
+import novasInativo from "../../public/icones/novas-inativo.png";
+import surpreendaAtivo from "../../public/icones/surpreenda-me-ativo.png";
+import surpreendaInativo from "../../public/icones/surpreenda-me-inativo.png";
 
-export default search;
+export {
+  search,
+  favoritoAtivo,
+  favoritoInativo,
+  homeAtivo,
+  homeInativo,
+  maisCurtidasAtivo,
+  maisCurtidasInativo,
+  maisVistasAtivo,
+  maisVistasInativo,
+  novasAtivo,
+  novasInativo,
+  surpreendaAtivo,
+  surpreendaInativo,
+};
